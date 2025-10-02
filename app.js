918e6018ee3f8e71388dca864d9f45afca9e44cf
Suleiman-baba <bmdanjumah@gmail.com>
2025-10-02 23:14:43 +0000

    added a new rooute cicd

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello from your dummy Node.js project!');
 });
+app.get('/cicd', (req, res) => {
+    res.send(`
+        You created a CI/CD flow but your understanding was less than 50%
+        Congratulations
+        `);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
